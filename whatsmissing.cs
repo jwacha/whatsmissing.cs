26d196ba85d2e6e661728888ef079619cb6eebf9
ryanbanana <ryanschell@live.com>
2015-01-11 08:29:49 +0000

    Initial commit

--- whatsmissing.cs
+++ whatsmissing.cs
@@ -8,16 +8,19 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using System.Windows.Controls;
+
 
 namespace whatsmissing
 {
     public partial class Form1 : Form
     {
+        public List<string> debug;
         public Dictionary<string, List<int>> indexTable;
+        public string HTMLToPublish;
         public Form1()
         {
             InitializeComponent();
+            debug = new List<string>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -69,6 +72,11 @@ namespace whatsmissing
             return (isQuoteOnEnd(stringToParse) && isQuoteOnFront(stringToParse));
         } // I'm keeping this in because it's beautiful even though I'm not using it. Fuck you.
 
+        private bool isSingleLetterWord(string stringToParse)
+        {
+            return (string.Equals(stringToParse,"a") || string.Equals(stringToParse,"i"));
+        }
+
         static string printMatches(bool[] matches, string[] primarySource)
         {
             string toPrint = "";
@@ -77,21 +85,66 @@ namespace whatsmissing
                 if (matches[i])
                 {
                     
-                    toPrint = toPrint + primarySource[i];
+                    toPrint = toPrint + primarySource[i] + " ";
                 }// ends if
             }// ends for
             return toPrint;
         }
 
+        static public bool[] removeFalsePositives(bool[] boolsToCheck)
+        {
+            bool trail0 = true;
+            bool trail1 = true;
+            bool trail2 = true;
+            bool trail3 = true;
+            for (int i = 0; i < boolsToCheck.Length; i++)
+            {
+                trail3 = trail2;
+                trail2 = trail1;
+                trail1 = trail0;
+                trail0 = boolsToCheck[i];
+                if (!trail0 && trail1 && !trail2){
+                    boolsToCheck[i - 1] = false;
+                } else if (!trail0 && trail1 && trail2 && !trail3){
+                    boolsToCheck[i - 1] = false;
+                    boolsToCheck[i - 2] = false;
+                }
+            }
+
+            return boolsToCheck;
+        }
+
+        static public string printAsHtml(bool[] matches, string[] primarySource)
+        {
+            string openStrike = "<span style=\"color:BlanchedAlmond; background-color:grey;\">";
+            string closeStrike = "</span>";
+            string toReturn = "";
+            Boolean previous = true;
+            for (int i = 0; i < matches.Length; i++)
+            {
+                if(!previous && matches[i]){
+                    toReturn = toReturn + closeStrike;
+                }
+
+                if (previous && !matches[i]){
+                    toReturn = toReturn + openStrike;
+                }
+                toReturn = toReturn + " " + primarySource[i];
+                previous = matches[i];
+            }
+            return toReturn;
+        }
+
         private void compare_Click(object sender, EventArgs e)
         {
+            webBrowser.DocumentText = "";
             string articleText = article.Text;
             string primarySourceText = primarySource.Text;
+            string[] cleanPrimarySource = Regex.Split(primarySourceText.Trim(), @"\s+"); // to be crossed out and printed.
             string pattern = @"\p{P}";
             Regex regex = new Regex(pattern);
             primarySourceText = regex.Replace(primarySourceText,"");
             articleText = regex.Replace(articleText, "");
-            string[] cleanPrimarySource = Regex.Split(primarySourceText.Trim(), @"\s+"); // to be crossed out and printed.
             string[] primarySourceArray = Regex.Split(primarySourceText.Trim().ToLower(), @"\s+");
             string[] articleTextArray = Regex.Split(articleText.Trim().ToLower(), @"\s+");
             bool[] matches = new bool[primarySourceArray.Length];
@@ -116,28 +169,61 @@ namespace whatsmissing
                         do {
                             segmentInPrimary = string.Join("",primarySourceArray,positionInPrimarySource,possibleMatchSize);
                             segmentInArticle = string.Join("",articleTextArray,positionInArticle,possibleMatchSize);
+                            //string lastWordInPrimary = primarySourceArray[positionInPrimarySource + possibleMatchSize];
+                            int safeIndexInPrimary = positionInPrimarySource + possibleMatchSize;
+                            int safeIndexInArticle = positionInArticle + possibleMatchSize;
+                            if (safeIndexInPrimary >= primarySourceArray.Length){
+                                safeIndexInPrimary = primarySourceArray.Length - 1;
+                            }
+                            if (safeIndexInArticle >= articleTextArray.Length){
+                                safeIndexInArticle = articleTextArray.Length - 1;
+                            }
+                            string lastWordInPrimary = primarySourceArray[safeIndexInPrimary-1];
+                            //MessageBox.Show(lastWordInPrimary);
+                            string lastWordInArticle = articleTextArray[safeIndexInArticle-1];
                             //MessageBox.Show("primary: " + segmentInPrimary + " " + "article: " + segmentInArticle);
                             editDistance = lDistance(segmentInArticle,segmentInPrimary);
-       
-                            if (possibleMatchSize > 3 && editDistance < 5){
+
+                            if (possibleMatchSize >= 2 && (editDistance < 2 && // changed positionInArticle
+                                (lastWordInPrimary.Length + lastWordInArticle.Length != 3 && 
+                                primarySourceArray[positionInPrimarySource + possibleMatchSize].Length > editDistance)) ||
+                                (isSingleLetterWord(lastWordInArticle) && (string.Equals(lastWordInArticle,lastWordInPrimary))))
+                                {
+                                    if (string.Equals(lastWordInArticle, "in") || string.Equals(lastWordInPrimary, "in")){
+         //MessageBox.Show("article: "+lastWordInArticle+"primary: "+lastWordInPrimary);
+     }
                                 for (int i = positionInPrimarySource; i < positionInPrimarySource + possibleMatchSize && i < matches.Length; i++)
                                 {
                                     matches[i] = true;
+                                 //   debug.Add(segmentInArticle + ":" + segmentInPrimary + ":" + i.ToString() + ",   ");
                                     //MessageBox.Show("match");
-                                }
-                            }
+                                }// ends for
+                            }// ends if
                             possibleMatchSize++;
-                        } while(editDistance < 5 && positionInArticle + possibleMatchSize < articleTextArray.Length && positionInPrimarySource + possibleMatchSize < primarySourceArray.Length);
+                        } while(editDistance < 2 && positionInArticle + possibleMatchSize < articleTextArray.Length && positionInPrimarySource + possibleMatchSize < primarySourceArray.Length);
+                        // this catches the last word.
+                        if (string.Equals(articleTextArray[articleTextArray.Length-1],primarySourceArray[primarySourceArray.Length-1])){
+                            matches[matches.Length - 1] = true;
 
+                        }
                     }// we get 
 
                 }// ends if
             }// ends for
 
             //printArticleText(primarySourceArray);
-
-            output.Text = printMatches(matches,primarySourceArray);
-
+            //.outputMessageBox.Show("matches" + matches.Length + "primarysource" + cleanPrimarySource.Length);
+            matches = removeFalsePositives(matches);
+            string myHTML = printAsHtml(matches,cleanPrimarySource);
+            //output.Text = myHTML;
+            string stringToPutInBox = "";
+            //foreach (string stringToAdd in debug)
+            //{
+            //    stringToPutInBox = stringToPutInBox + stringToAdd;
+            //}
+            webBrowser.DocumentText = myHTML;
+            //webBrowser.DocumentText = stringToPutInBox;
+            HTMLToPublish = myHTML;
             /*
             foreach(string key in indexTable.Keys){
                 output.Text = output.Text + " " + key + "   " + string.Join(",",indexTable[key].ToArray());
@@ -206,7 +292,7 @@ namespace whatsmissing
                 stringToPrint = stringToPrint + textToPrint[i];
                 stringToPrint = stringToPrint + "             ";
             }
-            output.Text = stringToPrint;
+            //output.Text = stringToPrint;
         }// ends method
 
         
@@ -272,6 +358,26 @@ namespace whatsmissing
                 newWord.ToLower();
                 word = new string(newWord.Where(c => !char.IsPunctuation(c)).ToArray());
                 isInQuotes = newIsInQuotes;
+            }
+        }
+
+        private void publish_Click(object sender, EventArgs e)
+        {
+            string stringToPublish = HTMLToPublish;
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "html file|*.html";
+            saveFile.Title = "Save the HTML to publish on web.";
+            saveFile.ShowDialog();
+            string nameOfSave = saveFile.FileName;
+            try
+            {
+                System.IO.StreamWriter writeStream = new System.IO.StreamWriter(nameOfSave);
+                writeStream.Write(stringToPublish);
+                writeStream.Close();
+            }
+            catch
+            {
+                // don't do anything.
             }
         }
     }
